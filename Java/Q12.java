c9745d98d063b856d1dec2c4671821b104f51297
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-17 19:22:24 +0530

    Create Q12.java

---
+++ Java/Q12.java
@@ -0,0 +1,4 @@
+/*Q12) Write a Java program that reads two integer number and create a third number
+by taking the first two digits of the first number and the last two digits of the
+second number.
+Example: Input: 45678, 312 Output:4512*/
