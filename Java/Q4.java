d75e7845b830595de5abaf1d915575a64199738d
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-17 19:16:25 +0530

    Create Q4.java

---
+++ Java/Q4.java
@@ -0,0 +1,4 @@
+/*Q4) Write a program that takes a 64-bit word and returns the 64-bit word consisting
+of the bits of the input word in reverse order. For example, if the input is
+alternating 1s and 0s, i.e., (1010...10), the output should be alternating 0s and
+1s, i.e.,(0101...01).*/
