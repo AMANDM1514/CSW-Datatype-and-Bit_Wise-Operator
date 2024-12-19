3157f1b4a9bd778e3e453a2c8748f03a9886bd8b
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-19 23:53:55 +0530

    Update Q11.java

--- Java/Q11.java
+++ Java/Q11.java
@@ -1,2 +1,24 @@
 /*Q11) Write a Java program that reads an integer number and counts the number of
 digits that are even.*/
+
+
+public class Q11 {
+    public static int countEvenDigits(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            int digit = Math.abs(n % 10);
+            if (digit % 2 == 0)
+            	count++;
+            n /= 10;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int n = 45678;
+        System.out.println("Number of even digits: " + countEvenDigits(n));
+    }
+}
+
