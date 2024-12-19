039c8539c3946967cc00d928cdef0755e2f7819d
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-20 00:15:51 +0530

    Update Q15.java

--- Java/Q15.java
+++ Java/Q15.java
@@ -1,1 +1,26 @@
 /*Q15) Write a program to print the first 100th prime number.*/
+
+
+public class Q15 {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int count = 0, number = 2;
+
+        System.out.println("First 100 prime numbers:");
+        while (count < 100) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+}
+
