f44758211801a7074bae7e556c267139531c8c35
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-19 23:52:34 +0530

    Update Q9.java

--- Java/Q9.java
+++ Java/Q9.java
@@ -1,1 +1,19 @@
 /*Q9) Write a program to check whether a number is palindrome or not.*/
+
+
+public class Q9 {
+    public static boolean isPalindrome(int n) {
+        int original = n, reversed = 0;
+
+        while (n != 0) {
+            reversed = reversed * 10 + (n % 10);
+            n /= 10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        int n = 165;
+        System.out.println(n + " is palindrome: " + isPalindrome(n));
+    }
+}
