b22a4e1ce708945b7965d48596e13e61de344d43
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-20 00:15:03 +0530

    Update Q13.java

--- Java/Q13.java
+++ Java/Q13.java
@@ -1,1 +1,24 @@
 /*Q13) Write a Java program to count the frequency of each digit of a number.*/
+
+
+public class Q13 {
+    public static void countDigitFrequency(int n) {
+        int[] freq = new int[10];
+        n = Math.abs(n); // Handle negatives
+
+        while (n != 0) {
+            int digit = n % 10;
+            freq[digit]++;
+            n /= 10;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > 0) System.out.println("Digit " + i + ": " + freq[i] + " times");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 122345;
+        countDigitFrequency(n);
+    }
+}
