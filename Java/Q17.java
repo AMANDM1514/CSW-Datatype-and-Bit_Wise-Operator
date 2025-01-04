d1c983a4bfd86fd97c7737498d66e9582c476b82
AMAN DEV MAHATO <amandevv007@gmail.com>
2025-01-04 22:59:43 +0530

    Update Q17.java

--- Java/Q17.java
+++ Java/Q17.java
@@ -6,17 +6,13 @@ import java.util.Scanner;
 
 public class EvenNumberCheck {
     public static boolean isEven(int num) {
-        return (num % 2) == 0; // Returns true if num is even, false otherwise
+        return (num % 2) == 0;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Input number
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
-        
-        // Check if the number is even and print the result
         System.out.println("Is the number even? " + isEven(number));
         
         scanner.close();
